b833dc0cc383e3d863cdd2615443d268b63b83b1
Bram Gussekloo <bram.gussekloo@gmail.com>
2026-07-30 19:25:02 +0200

    Initial Commit

---
+++ Container GUI/Container_GUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Container_GUIApp.swift
+//  Container GUI
+//
+//  Created by Bram Gussekloo on 30/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Container_GUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Container GUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Container GUI
+//
+//  Created by Bram Gussekloo on 30/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
